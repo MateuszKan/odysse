a97703ac0e0858fe7fc2aabf64b4eddc28594719
MateuszKan <120403321+MateuszKan@users.noreply.github.com>
2024-05-28 17:53:36 +0100

    new version

--- email.php
+++ email.php
@@ -1,45 +1,39 @@
 <?php
-//Import PHPMailer classes into the global namespace
-//These must be at the top of your script, not inside a function
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-//Load Composer's autoloader
 require 'vendor/autoload.php';
 
-//Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
 
-// Dane pobrane z formularza
 $name = $_POST['name'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
 $license = $_POST['license'];
-$experience = $_POST['experience'];
-$address = $_POST['address'];
+$rent = $_POST['rent'];
+$app = $_POST['app'];
 $referral = isset($_POST['referral']) ? $_POST['referral'] : '';
 
 try {
-    // Konfiguracja serwera SMTP
     $mail->isSMTP();
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
     $mail->Username = 'phpemail3@gmail.com';
     $mail->Password = 'xtml oxbo ppup npsm';
-    $mail->Port = 587; // Może być inny, zależnie od konfiguracji serwera SMTP
-    $mail->SMTPSecure = 'tsl'; // Szyfrowanie, np. ssl lub tls
+    $mail->Port = 587;
+    $mail->SMTPSecure = 'tls';
 
-    // Ustawienia wiadomości
     $mail->setFrom($email, $name);
-    $mail->addAddress('mateuszkantarewicz@gmail.com'); // Adres, na który zostanie wysłana wiadomość
+    $mail->addAddress('mateuszkantarewicz@gmail.com');
     $mail->isHTML(false);
     $mail->Subject = 'PCO Driver Application';
-    $mail->Body    = "Name: $name\nEmail: $email\nPhone: $phone\nDriver's License Number: $license\nDriving Experience: $experience years\nResidential Address: $address\nReferral: $referral";
+    $mail->Body    = "Name: $name\nEmail: $email\nMobile Number: $phone\nHow many years do you hold PCO license? $license years\nDo you rent or own the vehicle that you use for work?: $rent \nOn which taxi services or ride-hailing platforms do you drive?: $app\nReferral: $referral";
 
-    // Wysłanie wiadomości
     $mail->send();
-    echo 'Application submitted successfully';
+    header('Location: success.html');
+    exit();
 } catch (Exception $e) {
-    echo 'An error occurred while submitting the application: ', $mail->ErrorInfo;
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
--- emailgeneral.php
+++ emailgeneral.php
@@ -1,6 +1,5 @@
 <?php
 //Import PHPMailer classes into the global namespace
-//These must be at the top of your script, not inside a function
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
@@ -18,8 +17,7 @@ try {
     $mail->Username = 'phpemail3@gmail.com';
     $mail->Password = 'xtml oxbo ppup npsm';
     $mail->Port = 587; // Może być inny, zależnie od konfiguracji serwera SMTP
-    $mail->SMTPSecure = 'tsl'; // Szyfrowanie, np. ssl lub tls
-
+    $mail->SMTPSecure = 'tls'; // Szyfrowanie, np. ssl lub tls
 
     // Odbiorca
     $mail->setFrom($_POST['email'], $_POST['name']);
@@ -28,13 +26,14 @@ try {
     // Content
     $mail->isHTML(false);
     $mail->Subject = 'New Inquiry from Website';
-    $mail->Body    = 'Name: ' . $_POST['name'] . '\n' .
-        'Email: ' . $_POST['email'] . '\n' .
-        'Phone: ' . $_POST['phone'] . '\n' .
+    $mail->Body    = 'Name: ' . $_POST['name'] . "\n" .
+        'Email: ' . $_POST['email'] . "\n" .
+        'Phone: ' . $_POST['phone'] . "\n" .
         'Message: ' . $_POST['message'];
 
     $mail->send();
-    echo 'Message has been sent';
+    header('Location: success-general.html');
+    exit();
 } catch (Exception $e) {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
