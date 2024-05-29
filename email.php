69806cf4b4778268bda6440b879d3fe2fa055822
MateuszKan <120403321+MateuszKan@users.noreply.github.com>
2024-05-29 18:50:34 +0100

    added git action

--- email.php
+++ email.php
@@ -21,12 +21,12 @@ try {
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
     $mail->Username = 'botodysee@gmail.com';
-    $mail->Password = 'naku quiz njqe onvl';
+    $mail->Password = getenv('GMAIL_PASSWORD');
     $mail->Port = 587;
     $mail->SMTPSecure = 'tls';
 
     $mail->setFrom($email, $name);
-    $mail->addAddress('infoinfo@odysse.co.uk');
+    $mail->addAddress('info@odysse.co.uk');
     $mail->isHTML(false);
     $mail->Subject = 'PCO Driver Application';
     $mail->Body    = "Name: $name\nEmail: $email\nMobile Number: $phone\nHow many years do you hold PCO license? $license years\nDo you rent or own the vehicle that you use for work?: $rent \nOn which taxi services or ride-hailing platforms do you drive?: $app\nReferral: $referral";
