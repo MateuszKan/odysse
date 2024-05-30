5345f5aa8d0c930fc0c37653216d36f1a4b796bb
MateuszKan <120403321+MateuszKan@users.noreply.github.com>
2024-05-30 23:01:43 +0100

    added variables

--- email.php
+++ email.php
@@ -4,6 +4,7 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
+require './config.php';
 require 'vendor/autoload.php';
 
 $mail = new PHPMailer(true);
@@ -18,15 +19,15 @@ $referral = isset($_POST['referral']) ? $_POST['referral'] : '';
 
 try {
     $mail->isSMTP();
-    $mail->Host = 'smtp.gmail.com';
+    $mail->Host = SMTP_HOST;
     $mail->SMTPAuth = true;
-    $mail->Username = 'botodysee@gmail.com';
-    $mail->Password = getenv('GMAIL_PASSWORD');
+    $mail->Username = SMTP_USER;;
+    $mail->Password = SMTP_PASSWORD;
     $mail->Port = 587;
     $mail->SMTPSecure = 'tls';
 
     $mail->setFrom($email, $name);
-    $mail->addAddress('info@odysse.co.uk');
+    $mail->addAddress('mateuszkantarewicz@gmail.com');
     $mail->isHTML(false);
     $mail->Subject = 'PCO Driver Application';
     $mail->Body    = "Name: $name\nEmail: $email\nMobile Number: $phone\nHow many years do you hold PCO license? $license years\nDo you rent or own the vehicle that you use for work?: $rent \nOn which taxi services or ride-hailing platforms do you drive?: $app\nReferral: $referral";
